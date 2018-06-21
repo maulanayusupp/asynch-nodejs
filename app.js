9d390afe36295882c10ebf88d0270248cc3b280c
Maulana Yusup Abdullah <adeneling@gmail.com>
2018-06-21 12:34:18 +0700

    Encoding User Input

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv  = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 var params = {
-	url: "http://maps.googleapis.com/maps/api/geocode/json?address=Bandung",
+	url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json: true
 }
 request(params, (error, response, body) => {
